1a2f65ab38e121e038adecb40e66c06a4be231e0
Maksym Kotyk <mxm.kotyk@gmail.com>
2023-02-23 20:46:14 +0100

    Remade second task with function

--- js/task-02.js
+++ js/task-02.js
@@ -1,14 +1,15 @@
 const ingredients = ["Potatoes", "Mushrooms", "Garlic", "Tomatos", "Herbs", "Condiments"];
 
-/*Solution*/
+const makeIngredientsItems = ingredientsList => {
+	return ingredientsList.map(ingredient => {
+		const ingredientItem = document.createElement("li");
+		ingredientItem.textContent = `${ingredient}`;
+		ingredientItem.classList.add("item");
+		return ingredientItem;
+	});
+};
 
-const ingredientsItems = ingredients.map(ingredient => {
-	const ingredientItem = document.createElement("li");
-	ingredientItem.textContent = `${ingredient}`;
-	ingredientItem.classList.add("item");
-	return ingredientItem;
-});
-
+const ingredientsItems = makeIngredientsItems(ingredients);
 const ingredientsList = document.querySelector("#ingredients");
 
 ingredientsList.append(...ingredientsItems);

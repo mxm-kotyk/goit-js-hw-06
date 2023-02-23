f12575ba656a4295312faed37aa706e4384b3859
Maksym Kotyk <mxm.kotyk@gmail.com>
2023-02-23 13:35:46 +0100

    First task done

---
+++ js/task-01.js
@@ -0,0 +1,27 @@
+/*Number of categories*/
+const categoriesEl = document.querySelector("#categories").children;
+console.log(`Number of categories: ${categoriesEl.length}`);
+
+/*Animals list*/
+const animalsTitleEl = document.querySelector("#categories").firstElementChild.querySelector("h2");
+
+const animalsListEl = document.querySelector("#categories").firstElementChild.querySelectorAll("li");
+console.log(`Category: ${animalsTitleEl.textContent}
+Elements: ${animalsListEl.length}`);
+
+/*Products list*/
+const productsTitleEl = document.querySelector("#categories").firstElementChild.nextElementSibling.querySelector("h2");
+console.log(productsTitleEl.textContent);
+
+const productsListEl = document
+	.querySelector("#categories")
+	.firstElementChild.nextElementSibling.querySelectorAll("li");
+console.log(`Category: ${productsTitleEl.textContent}
+Elements: ${productsListEl.length}`);
+
+/*Technologies list*/
+const technologiesTitleEl = document.querySelector("#categories").lastElementChild.querySelector("h2");
+
+const technologiesListEl = document.querySelector("#categories").lastElementChild.querySelectorAll("li");
+console.log(`Category: ${technologiesTitleEl.textContent}
+Elements: ${technologiesListEl.length}`);

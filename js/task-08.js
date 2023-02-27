142d52c2215772acb325a5dd62e31a28a099bff7
Maksym Kotyk <mxm.kotyk@gmail.com>
2023-02-27 17:40:42 +0100

    Eighth task done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const form = document.querySelector(".login-form");
 
+const handleFormSubmit = event => {
+	event.preventDefault();
+
+	const formData = {
+		email: event.currentTarget.elements.email.value,
+		password: event.currentTarget.elements.password.value,
+	};
+
+	if (formData.email === "" || formData.password === "") {
+		alert("Please fill in all the fields!");
+	} else {
+		console.log(formData);
+		event.currentTarget.reset();
+	}
+};
+
+form.addEventListener("submit", handleFormSubmit);

be0a36537edcb57f77a63c2f801054a5846cb7ab
Maksym Kotyk <mxm.kotyk@gmail.com>
2023-02-27 17:51:15 +0100

    Ninth task done

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,17 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+	return `#${Math.floor(Math.random() * 16777215)
+		.toString(16)
+		.padStart(6, 0)}`;
 }
+
+/* Solution */
+
+const changeColorBtn = document.querySelector(".change-color");
+const colorDisplay = document.querySelector(".color");
+
+const handleColorChangeOnClick = event => {
+	document.body.style.background = getRandomHexColor();
+	colorDisplay.textContent = getRandomHexColor();
+};
+
+changeColorBtn.addEventListener("click", handleColorChangeOnClick);

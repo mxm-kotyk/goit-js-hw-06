1e28c7d5d5cef01ca910362762187e86fbab3d87
Maksym Kotyk <mxm.kotyk@gmail.com>
2023-02-28 13:42:16 +0100

    First task refractoring

--- js/task-01.js
+++ js/task-01.js
@@ -4,23 +4,27 @@ console.log(`Number of categories: ${categoriesEl.length}`);
 
 /*Animals list*/
 const animalsTitleEl = document.querySelector("#categories").firstElementChild.querySelector("h2");
-
 const animalsListEl = document.querySelector("#categories").firstElementChild.querySelectorAll("li");
-console.log(`Category: ${animalsTitleEl.textContent}
-Elements: ${animalsListEl.length}`);
 
 /*Products list*/
 const productsTitleEl = document.querySelector("#categories").firstElementChild.nextElementSibling.querySelector("h2");
-
 const productsListEl = document
 	.querySelector("#categories")
 	.firstElementChild.nextElementSibling.querySelectorAll("li");
-console.log(`Category: ${productsTitleEl.textContent}
-Elements: ${productsListEl.length}`);
 
 /*Technologies list*/
 const technologiesTitleEl = document.querySelector("#categories").lastElementChild.querySelector("h2");
-
 const technologiesListEl = document.querySelector("#categories").lastElementChild.querySelectorAll("li");
-console.log(`Category: ${technologiesTitleEl.textContent}
-Elements: ${technologiesListEl.length}`);
+
+
+const logCategory = (title, list) => {
+	console.log(`Category: ${title.textContent}
+Elements: ${list.length}`);
+}
+
+logCategory(animalsTitleEl, animalsListEl)
+logCategory(productsTitleEl, productsListEl)
+logCategory(technologiesTitleEl, technologiesListEl)
+
+
+

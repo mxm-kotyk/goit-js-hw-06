e17bab3838439a78aa904c2b83996a1e7e04c0b9
Maksym Kotyk <mxm.kotyk@gmail.com>
2023-03-06 18:38:24 +0100

    Refractored first task some more

--- js/task-01.js
+++ js/task-01.js
@@ -1,30 +1,8 @@
-/*Number of categories*/
-const categoriesEl = document.querySelector("#categories").children;
-console.log(`Number of categories: ${categoriesEl.length}`);
+const categoriesItems = document.querySelectorAll(".item");
 
-/*Animals list*/
-const animalsTitleEl = document.querySelector("#categories").firstElementChild.querySelector("h2");
-const animalsListEl = document.querySelector("#categories").firstElementChild.querySelectorAll("li");
+console.log(`Number of categories: ${categoriesItems.length}`);
 
-/*Products list*/
-const productsTitleEl = document.querySelector("#categories").firstElementChild.nextElementSibling.querySelector("h2");
-const productsListEl = document
-	.querySelector("#categories")
-	.firstElementChild.nextElementSibling.querySelectorAll("li");
-
-/*Technologies list*/
-const technologiesTitleEl = document.querySelector("#categories").lastElementChild.querySelector("h2");
-const technologiesListEl = document.querySelector("#categories").lastElementChild.querySelectorAll("li");
-
-
-const logCategory = (title, list) => {
-	console.log(`Category: ${title.textContent}
-Elements: ${list.length}`);
-}
-
-logCategory(animalsTitleEl, animalsListEl)
-logCategory(productsTitleEl, productsListEl)
-logCategory(technologiesTitleEl, technologiesListEl)
-
-
-
+categoriesItems.forEach(category =>
+	console.log(`Category: ${category.firstElementChild.textContent}
+Elements: ${category.lastElementChild.children.length}`),
+);
